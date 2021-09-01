bd7fda70eeb5103f9712b8221e9cda1d96ed6991
patrickjane25 <anshumandas07657@gmail.com>
2021-09-02 02:34:08 +0530

    Description of commit

---
+++ ex3-octave/lrCostFunction.m
@@ -0,0 +1,57 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+h = sigmoid(X*theta);
+theta1 = [0 ; theta(2:end, :)]; %as we know we don't regularize theta(0) hence in the operations we want to remove the term theta(0) hence we make the theta matrix as theta(2:end,:) which means theta is the matrix with rows from 2 to end (as theta(0) is in the first row) and all the columns 
+p = lambda*(theta1'*theta1)/(2*m);
+J = ((-y)'*log(h) - (1-y)'*log(1-h))/m + p;
+grad = (X'*(h - y)+lambda*theta1)/m; %it is the differential of cost function
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
